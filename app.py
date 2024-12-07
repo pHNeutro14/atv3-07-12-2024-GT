667c9c0eaef2a5fe47bad886adc215f1045cde14
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2024-12-07 18:23:10 +0000

    Fiz muita coisa socorro

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/fazerpedido')
+def fazerpedido():
+    return render_template('fazerpedido.html')
+
+@app.route('/confirmar_pedido', methods=["POST"])
+def confirmar_pedido():
+    recheio = request.form.get('recheio')
+    molho = request.form.get('molho')
+    massa = request.form.get('massa')
+    return render_template('confirmar_pedido.html', recheio = recheio, molho = molho, massa = massa)
